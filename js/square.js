8cb1655cb3d9e30326071d9bfa8b22730a150eee
apple <upingshen@gmail.com>
2018-07-27 16:51:30 +0800

    squareFactory

--- js/square.js
+++ js/square.js
@@ -1,38 +1,14 @@
 var   Square=function(){
-  //square
+
+  //Square
   this.data=[
-    [0,2,0,0],
-    [0,2,0,0],
-    [0,2,0,0],
-    [0,2,0,0]
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
   ];
   this.dir=0;
-  this.uprotate=[
-    [
-      [0,2,0,0],
-      [0,2,0,0],
-      [0,2,0,0],
-      [0,2,0,0]
-    ],
-    [
-      [0,0,0,0],
-      [2,2,2,2],
-      [0,0,0,0],
-      [0,0,0,0]
-    ],
-    [
-      [0,2,0,0],
-      [0,2,0,0],
-      [0,2,0,0],
-      [0,2,0,0]
-    ]
-    ,[
-      [0,0,0,0],
-      [2,2,2,2],
-      [0,0,0,0],
-      [0,0,0,0]
-    ]
-  ]
+
   this.origin={
     x:0,
     y:0
@@ -84,14 +60,14 @@ Square.prototype.right=function(){
   this.origin.y+=1;
 }
 Square.prototype.up=function(){
-  var d=this.dir+1;
+  this.dir=this.dir+1;
 
-  if(d==4){
-    d=0;
+  if(this.dir==4){
+    this.dir=0;
   }
   for(var i=0;i<this.data.length;i++){
     for(var j=0;j<this.data[0].length;j++){
-      this.data[i][j]=this.uprotate[d][i][j];
+      this.data[i][j]=this.uprotate[this.dir][i][j];
     }
   }
 
