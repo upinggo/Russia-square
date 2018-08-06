2e8e8a3e86933495155f8a9773f3b70c905b9529
apple <upingshen@gmail.com>
2018-08-06 15:29:45 +0800

    imooc

--- js/http/imooc.js
+++ js/http/imooc.js
@@ -3,9 +3,9 @@ var url='http://www.imooc.com/learn/384';
 var cheerio=require('cheerio')
 function filterChapters(html){
     var $=cheerio.load(html);
-    var chapters=$('.learnchapter');
+    var chapters=$('.chapter');
     // [{
-    //     chapterTite:'',
+    //     chapterTitle:'',
     //     video:[
     //         title : '',
     //         id:'',
@@ -14,9 +14,25 @@ function filterChapters(html){
     var courseData=[];
     chapters.each(function(item){
         var chapter=$(this);
-        var chapterTitle=chapter.find("strong").text();
-        var video
+        var chapterTitle=chapter.find(".chapter-description").text();
+        var videos=chapter.find('.video').children('li')
+        var chapterData={
+            chapterTitle:chapterTitle,
+            videos:[]
+        };
+        videos.each(function(item){
+            var video=$(this).find('.J-media-item');
+            var videoTitle=video.text();
+            var id=video.attr('href').split('video/')[1];
+            chapterData.videos.push({title:videoTitle,
+            id:id,
+            })
+
+        })
+        courseData.push(chapterData)
     })
+    return courseData;
+    
 }
 
 
@@ -27,9 +43,19 @@ http.get(url,function(res){
     })
 
     res.on('end',function(){
-        filterChapters(html)
+        var courseData=filterChapters(html);
+        printInfo(courseData);
     }).on('error',function(){
         console.log("出错啦！！！")
     });
 
 })
+function printInfo(courseData){
+    courseData.forEach(function(item){
+        var chapterTitle=item.chapterTitle;
+        console.log(chapterTitle+'\n');
+        item.videos.forEach(function(item){
+            console.log("【"+item.id+"】"+item.title+"\n");
+        });
+    })
+}
